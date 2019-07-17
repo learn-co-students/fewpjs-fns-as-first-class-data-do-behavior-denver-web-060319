10bb0942c39dc48621c8edf5fa203a95488457c9
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-07-17 14:31:26 -0600

    Create JS callback functions

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+const greet = function(time) {
+  if (time.slice(0,2) >= 17) {
+    return 'Good Evening';
+  } else if (time.slice(0,2) >= 12 && time.slice(0,2) < 17) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Morning';
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+const displayMessage = function(string) {
+  const greeting = document.getElementById('greeting')
+  greeting.innerText = `${string}`
+}
